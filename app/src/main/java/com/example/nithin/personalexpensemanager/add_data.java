91937145dec7d2c278210a0c6d1d6c65d15de889
nithinjacob111 <nithinjacob27@gmail.com>
2017-08-27 23:05:19 +0530

    test commit

---
+++ app/src/main/java/com/example/nithin/personalexpensemanager/add_data.java
@@ -0,0 +1,13 @@
+package com.example.nithin.personalexpensemanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class add_data extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_data);
+    }
+}

624985390f33fdb453634ef00440538e735e7e8a
nithinjacob111 <nithinjacob27@gmail.com>
2017-08-27 23:21:56 +0530

    27-08-2017 11:21pm

--- app/src/main/java/com/example/nithin/personalexpensemanager/HomeScreen.java
+++ app/src/main/java/com/example/nithin/personalexpensemanager/HomeScreen.java
@@ -1,5 +1,6 @@
 package com.example.nithin.personalexpensemanager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -29,6 +30,8 @@ public class HomeScreen extends AppCompatActivity
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Intent i=new Intent(HomeScreen.this,add_data.class);
+                startActivity(i);
             }
         });
 
